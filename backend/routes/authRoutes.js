5ce094374aba5571227abba340efcaf0ae337a25
karan <codedcup@gmail.com>
2025-04-12 18:13:46 +0530

    backend removed

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/admin/login', authController.loginAdmin);
-router.post('/teacher/login', authController.loginTeacher);
-router.post('/student/login', authController.loginStudent);
-
-module.exports = router;
